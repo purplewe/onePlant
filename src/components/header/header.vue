de5b4ce92b51258af31a1906c3d884715cb88f71
purplewe <647823223@qq.com>
2018-05-26 10:09:08 +0800

    5/26

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,19 +1,22 @@
 <template>
     <div>
-        <x-header class="Xheader" style="height:44px;" id="navhead" :left-options="{showBack: arrowFlag,backText: ''}">
+        <x-header :title="title" class="Xheader" style="height:44px;" id="navhead" :left-options="{showBack: arrowFlag,backText: ''}">
         </x-header>
-        <search v-if="showSearch"
-        @result-click="resultClick"
-        @on-change="getResult"
-        :results="results"
-        v-model="value"
-        position="absolute"
-        auto-scroll-to-top
-        @on-focus="onFocus"
-        @on-cancel="onCancel"
-        @on-submit="onSubmit"
-        ref="search">
-        </search>
+        <section v-if="flag">
+          <search
+          @result-click="resultClick"
+          @on-change="getResult"
+          :results="results"
+          v-model="value"
+          position="absolute"
+          auto-scroll-to-top
+          @on-focus="onFocus"
+          @on-cancel="onCancel"
+          @on-submit="onSubmit"
+          ref="search">
+          </search>
+          </section>
+        <div v-if="flag">{{flag}}</div>
     </div>
 </template>
 <style lang="less">
@@ -24,12 +27,21 @@ import { XHeader, Search } from "vux";
 export default {
   components: {
     XHeader,
-    Search
+    Search,
   },
+  // props: ['flag', 'title'],
+  props: 
+    {
+    flag: {
+      default: false
+    },
+    title: {
+      default: "词条"
+    }
+    },
   data() {
     return {
       arrowFlag: true,
-      showSearch: true,
       value: "水仙"
     };
   }
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,26 +1,51 @@
 <template>
     <div>
-        <x-head></x-head>
+        <x-head :title="title" :flag="flag"></x-head>
         <div>
           <router-view>
           </router-view>
         </div>
-        <tabbar></tabbar>
+        <tabbar>
+            <tabbar-item @on-item-click="changeTitleToItem" selected link="/showItem">
+                <img slot="icon" src="@/assets/icon/detail.png"/>
+                <img slot="icon-active" src="@/assets/icon/over-detail.png"/>
+                <span slot="label">词条</span>
+            </tabbar-item>
+            <tabbar-item @on-item-click="changeTitleToPotted" link="/pottedPlant">
+                <img slot="icon" src="@/assets/icon/flower.png"/>
+                <img slot="icon-active" src="@/assets/icon/over-flower.png"/>
+                <span slot="label">盆栽</span>
+            </tabbar-item>
+            <tabbar-item @on-item-click="changeTitleToAboutMe" link="/profile">
+                <img slot="icon" src="@/assets/icon/mine.png"/>
+                <img slot="icon-active" src="@/assets/icon/over-mine.png"/>
+                <span slot="label">我的</span>
+            </tabbar-item>
+        </tabbar>
     </div>
 </template>
 <script>
-import Tabbar from "@/components/tab/tabbar";
 import XHead from "@/components/header/header";
+import { Tabbar, TabbarItem } from "vux";
 export default {
   mounted(){
     this.$router.push({path: '/showItem'})
   },
   components: {
     Tabbar,
+    TabbarItem,
     XHead
   },
+  data(){
+    return{
+      title: "词条",
+      flag: false
+    }
+  },
   methods:{
-
+    changeTitleToItem(){this.title="词条", this.flag=true},
+    changeTitleToPotted(){this.title="我的盆栽"},
+    changeTitleToAboutMe(){this.title="个人"}
   }
 };
 </script>
